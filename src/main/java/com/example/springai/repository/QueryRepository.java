895b9cc7122a8d77ceae0447915e00231156c40c
sivayapps <2403281+sivayapps@users.noreply.github.com>
2025-03-25 21:33:06 -0600

    cleanup
    Cleanup of duplicate files

--- src/main/java/com/example/springai/repository/QueryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springai.repository;
-
-import com.example.springai.entity.Query;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QueryRepository extends JpaRepository<Query, Long> {
-}
